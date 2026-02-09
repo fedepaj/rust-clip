ab3c652bcfbded93f90638b3a770abcc26e18e76
Federico Paglioni <paglioni.federico@gmail.com>
2026-02-09 20:22:42 +0100

    win ble commit

--- src/transport/ble/windows.rs
+++ src/transport/ble/windows.rs
@@ -205,6 +205,7 @@ pub async fn start_ble_service(_identity: RingIdentity, tx_packet: Sender<WirePa
                                                                      
                                                                      if let Some(ch) = char_to_use {
                                                                          println!("🎯 [BLE-Win-Client] Write Char Found!");
+                                                                         let mut lock = state.lock().unwrap();
                                                                          lock.device = Some(device);
                                                                          lock.write_char = Some(ch.clone());
                                                                          
